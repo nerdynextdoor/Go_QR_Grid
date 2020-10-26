588cfcbc7dcb75ada0d27934cfa57483e5f6c1d5
yolamu <32828093+yolamu@users.noreply.github.com>
2020-10-27 01:03:38 +0800

    maingui output fix

--- mainGui.go
+++ mainGui.go
@@ -15,65 +15,58 @@ func main() {
 
 	//txtBoxMinWidth := 100
 	//txtBoxMinHeight := 20
-//
+	//
 	//txtBoxMaxWidth := 100
 	//txtBoxMaxHeight := 20
 
 	MainWindow{
 		Title:   "QRProject",
-		MinSize:  Size{200, 200},
-		Size:     Size{200, 200},
-		Layout:   VBox{MarginsZero: true},
+		MinSize: Size{200, 200},
+		Size:    Size{200, 200},
+		Layout:  VBox{MarginsZero: true},
 		Children: []Widget{
 			VSplitter{
 				Children: []Widget{
 					HSplitter{
-						MaxSize: Size{200,20}, MinSize: Size{200,20},
-							Children: []Widget{ 
-								TextLabel{Text: "  number Of QR codes",TextAlignment: AlignHNearVCenter, MaxSize: Size{130,15}, MinSize: Size{130,15}},
-								TextEdit{AssignTo: &numberOfQRcodes , MaxSize: Size{50,15}, MinSize: Size{50,15}},
-							},
-					
+						MaxSize: Size{200, 20}, MinSize: Size{200, 20},
+						Children: []Widget{
+							TextLabel{Text: "  number Of QR codes", TextAlignment: AlignHNearVCenter, MaxSize: Size{130, 15}, MinSize: Size{130, 15}},
+							TextEdit{AssignTo: &numberOfQRcodes, MaxSize: Size{50, 15}, MinSize: Size{50, 15}},
+						},
 					},
 
 					HSplitter{
-						MaxSize: Size{200,20}, MinSize: Size{200,20},
-							Children: []Widget{ 
-								TextLabel{Text: "  QR Width", TextAlignment: AlignHNearVCenter, MaxSize: Size{130,15}, MinSize: Size{130,15}},
-								TextEdit{AssignTo: &QRWidth , MaxSize: Size{50,15}, MinSize: Size{50,15}},
-							},
-					
+						MaxSize: Size{200, 20}, MinSize: Size{200, 20},
+						Children: []Widget{
+							TextLabel{Text: "  QR Width", TextAlignment: AlignHNearVCenter, MaxSize: Size{130, 15}, MinSize: Size{130, 15}},
+							TextEdit{AssignTo: &QRWidth, MaxSize: Size{50, 15}, MinSize: Size{50, 15}},
+						},
 					},
 
 					HSplitter{
-						MaxSize: Size{200,20}, MinSize: Size{200,20},
-							Children: []Widget{ 
-								TextLabel{Text: "  QR Height", TextAlignment: AlignHNearVCenter, MaxSize: Size{130,15}, MinSize: Size{130,15}},
-								TextEdit{AssignTo: &QRHeight , MaxSize: Size{50,15}, MinSize: Size{50,15}},
-							},
-					
+						MaxSize: Size{200, 20}, MinSize: Size{200, 20},
+						Children: []Widget{
+							TextLabel{Text: "  QR Height", TextAlignment: AlignHNearVCenter, MaxSize: Size{130, 15}, MinSize: Size{130, 15}},
+							TextEdit{AssignTo: &QRHeight, MaxSize: Size{50, 15}, MinSize: Size{50, 15}},
+						},
 					},
 
 					HSplitter{
-						MaxSize: Size{200,20}, MinSize: Size{200,20},
-							Children: []Widget{ 
-								TextLabel{Text: "  Background Image Width", TextAlignment: AlignHNearVCenter, MaxSize: Size{130,20}, MinSize: Size{130,20}},
-								TextEdit{AssignTo: &bgWidth , MaxSize: Size{50,15}, MinSize: Size{50,15}},
-							},
-					
+						MaxSize: Size{200, 20}, MinSize: Size{200, 20},
+						Children: []Widget{
+							TextLabel{Text: "  Background Image Width", TextAlignment: AlignHNearVCenter, MaxSize: Size{130, 20}, MinSize: Size{130, 20}},
+							TextEdit{AssignTo: &bgWidth, MaxSize: Size{50, 15}, MinSize: Size{50, 15}},
+						},
 					},
 
 					HSplitter{
-						MaxSize: Size{200,20}, MinSize: Size{200,20},
-							Children: []Widget{ 
-								TextLabel{Text: "  Background Image Height",TextAlignment: AlignHNearVCenter, MaxSize: Size{130,20}, MinSize: Size{130,20}},
-								TextEdit{AssignTo: &bgHeight , MaxSize: Size{50,15}, MinSize: Size{50,15}},
-							},
-					
+						MaxSize: Size{200, 20}, MinSize: Size{200, 20},
+						Children: []Widget{
+							TextLabel{Text: "  Background Image Height", TextAlignment: AlignHNearVCenter, MaxSize: Size{130, 20}, MinSize: Size{130, 20}},
+							TextEdit{AssignTo: &bgHeight, MaxSize: Size{50, 15}, MinSize: Size{50, 15}},
+						},
 					},
-					
-					
-					
+
 					//TextEdit{AssignTo: &outTE, ReadOnly: true},
 				},
 			},
@@ -101,8 +94,8 @@ func main() {
 						log.Fatal(err)
 					}
 
-					l.CreatingQRgrid(numQR ,QRw ,QRh , BGw, BGh)
-				//	outTE.SetText(strings.ToUpper(inTE.Text()))
+					l.CreatingQRgrid(numQR, QRw, QRh, BGw, BGh, "./")
+					//	outTE.SetText(strings.ToUpper(inTE.Text()))
 				},
 			},
 		},
