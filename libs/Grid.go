cb5c8af7f5874f5a89c69f2311e361b670a79270
yolamu <32828093+yolamu@users.noreply.github.com>
2020-10-27 00:51:48 +0800

    Added file path output

--- libs/Grid.go
+++ libs/Grid.go
@@ -10,26 +10,23 @@ import (
 	gim "github.com/ozankasikci/go-image-merge"
 )
 
-
-
-func CreatingQRgrid(numberOfQRcodes, QRWidth, QRHeight, bgWidth, bgHeight int) {
+func CreatingQRgrid(numberOfQRcodes, QRWidth, QRHeight, bgWidth, bgHeight int, OutputPath string) {
 
 	num := numberOfQRcodes
-	CreatingBackground(bgWidth,bgHeight)
-	QRcode(num,QRWidth,QRHeight)
-	
+	CreatingBackground(bgWidth, bgHeight)
+	QRcode(num, QRWidth, QRHeight)
+
 	counter := num * num
 	grids := []*gim.Grid{}
 	backgroundimage := "./out/image.png"
 	var QRimage string
-	
-	setXoffset, setYoffset := settingOffset(QRWidth,QRHeight,bgWidth,bgHeight)
-	
+
+	setXoffset, setYoffset := settingOffset(QRWidth, QRHeight, bgWidth, bgHeight)
+
 	fmt.Println("\nDrawing Grid")
 	for x := 0; x < counter; x++ {
 
-		
-		QRimage = "./out/test"+ strconv.Itoa(x) +".png"
+		QRimage = "./out/test" + strconv.Itoa(x) + ".png"
 		in := []*gim.Grid{
 
 			{
@@ -57,39 +54,36 @@ func CreatingQRgrid(numberOfQRcodes, QRWidth, QRHeight, bgWidth, bgHeight int) {
 	).Merge()
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 
-
-	
 	fmt.Println("Done!! \nSaving Image......")
 	// save the output to jpg or png
-	file, err := os.Create("./QRgrid.png")
+	file, err := os.Create(OutputPath + "/QRgrid.png")
 	err = jpeg.Encode(file, rgba, &jpeg.Options{Quality: 80})
 	err = png.Encode(file, rgba)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 	fmt.Println("Done!! \nYou can now open the new image called: QRgrid.png")
 
 }
 
-func settingOffset(QRWidth, QRHeight, bgWidth, bgHeight int)(int, int){
+func settingOffset(QRWidth, QRHeight, bgWidth, bgHeight int) (int, int) {
 	x := 0
 	y := 0
-	if (bgWidth-QRWidth)<0{
+	if (bgWidth - QRWidth) < 0 {
 		x = 0
-	}else{
-		x = (bgWidth-QRWidth)/2
+	} else {
+		x = (bgWidth - QRWidth) / 2
 	}
-		
 
-	if (bgHeight-QRHeight)<0{
+	if (bgHeight - QRHeight) < 0 {
 		y = 0
-	}else{
-		y = (bgHeight-QRHeight)/2
+	} else {
+		y = (bgHeight - QRHeight) / 2
 	}
-		
 
-		return x, y
+	return x, y
 }
-
